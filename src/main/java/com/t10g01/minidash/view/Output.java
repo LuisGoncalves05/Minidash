a5824fef0e425f4584f7e8b59dbde8e22caf4f17
Sofia Sousa <sofiasaesousa@gmail.com>
2024-11-17 18:48:27 +0000

    Fixes to Output interface

--- src/main/java/com/t10g01/minidash/view/Output.java
+++ src/main/java/com/t10g01/minidash/view/Output.java
@@ -1,15 +1,16 @@
 package com.t10g01.minidash.view;
 
-
 import com.t10g01.minidash.shared.Color;
 import com.t10g01.minidash.shared.Position;
+
+import java.io.IOException;
 
 public interface Output {
 
     void clear();
 
-    void drawPixel(Position position, Color color);
+    void drawPixel(int x, int y, Color color);
 
-    void refresh();
+    void refresh() throws IOException;
 
 }
