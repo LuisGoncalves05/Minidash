c657a2e318d8efe1d3d71c8e938a243155298990
bogosorter <luiswbarbosa@gmail.com>
2024-11-17 18:07:25 +0000

    PlayerController Skeleton

---
+++ src/main/java/com/t10g01/minidash/controller/PlayerController.java
@@ -0,0 +1,23 @@
+package com.t10g01.minidash.controller;
+
+import com.t10g01.minidash.model.Player;
+
+public class PlayerController {
+    private final Player player;
+
+    public PlayerController(Player player) {
+        this.player = player;
+    }
+
+    public void update(double deltaTime) {
+
+    }
+
+    public void jump(double height, double time) {
+
+    }
+
+    public void setGrounded(double groundHeight) {
+
+    }
+}
--- src/main/java/com/t10g01/minidash/model/Player.java
+++ src/main/java/com/t10g01/minidash/model/Player.java
@@ -4,17 +4,25 @@ import com.t10g01.minidash.shared.Position;
 
 public class Player {
     private Position position;
-    private Position previousPosition;
+    private Position previousPosition = new Position(0, 0);
 
     public Player(double x, double y) {
         Position position = new Position(x, y);
     }
 
-    public void update() {
-
+    public Position getPosition() {
+        return position;
     }
 
-    public void jump(double height, double time) {
+    public Position getPreviousPosition() {
+        return previousPosition;
+    }
 
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public void setPreviousPosition(Position previousPosition) {
+        this.previousPosition = previousPosition;
     }
 }
