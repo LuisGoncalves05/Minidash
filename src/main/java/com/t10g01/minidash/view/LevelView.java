0b666932fae517138db5d7610ae7e0b364b7d9f9
Sofia Sousa <sofiasaesousa@gmail.com>
2024-11-23 17:30:37 +0000

    Tests and implementation for visitBlock

--- src/main/java/com/t10g01/minidash/view/LevelView.java
+++ src/main/java/com/t10g01/minidash/view/LevelView.java
@@ -16,17 +16,39 @@ public class LevelView extends View<LevelModel> implements CollidableVisitor {
 
     public LevelView(LevelModel model, IOAdapter ioAdapter, GameSettings gameSettings) {
         super(model, ioAdapter);
-        this.gameSettings = gameSettings;
         int cameraWidth = gameSettings.getCameraWidth();
+        this.gameSettings = gameSettings;
         this.cameraOffset = cameraWidth * 0.4;
     }
 
     public void draw() {
+        for(Collidable collidable: model.getCollidables()) {
+            collidable.accept(this);
+        }
 
+        // TODO: draw player
     }
 
     public void visitBlock(Block block) {
-        //draw block
+        Position position = block.getPosition();
+        int resolution = gameSettings.getResolution();
+        double x = position.getX() - model.getPlayer().getPosition().getX() + cameraOffset;
+        double y = position.getY();
+
+        int x_pixels = (int)(x * resolution);
+        int y_pixels = (int)(y * resolution);
+
+        if(x <= -1 || x >= gameSettings.getCameraWidth()) return;
+
+        if (x < 0) {
+            int blockWidth = resolution + x_pixels;
+            ioAdapter.drawRectangle(0, y_pixels, blockWidth, resolution, gameSettings.getBlockColor());
+        } else if(x > gameSettings.getCameraWidth() - 1) {
+            int blockWidth = gameSettings.getCameraWidth() * resolution - x_pixels;
+            ioAdapter.drawRectangle(x_pixels, y_pixels, blockWidth, resolution, gameSettings.getBlockColor());
+        } else {
+            ioAdapter.drawRectangle(x_pixels, y_pixels, resolution, resolution, gameSettings.getBlockColor());
+        }
     }
 
 }
