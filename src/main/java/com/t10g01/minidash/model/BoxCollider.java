8acbde6d114911cb0c56628a5fc926748c2d8c86
bogosorter <luiswbarbosa@gmail.com>
2024-11-25 12:27:28 +0000

    BoxCollider skeletons

---
+++ src/main/java/com/t10g01/minidash/model/BoxCollider.java
@@ -0,0 +1,15 @@
+package com.t10g01.minidash.model;
+
+public class BoxCollider {
+    public BoxCollider(int x, int y, int width, int height) {
+
+    }
+
+    public boolean collision(Player player) {
+        return false;
+    }
+
+    public boolean topCollision(Player player) {
+        return false;
+    }
+}
