5f6de8a0ed8eac28a24ca1d1567469013b47631b
bogosorter <luiswbarbosa@gmail.com>
2024-11-25 13:03:56 +0000

    BoxCollider implementation

--- src/main/java/com/t10g01/minidash/model/BoxCollider.java
+++ src/main/java/com/t10g01/minidash/model/BoxCollider.java
@@ -1,15 +1,27 @@
 package com.t10g01.minidash.model;
 
 public class BoxCollider {
-    public BoxCollider(int x, int y, double width, double height) {
+    private final Vector2D lowerLeft;
+    private final Vector2D upperRight;
 
+    public BoxCollider(int x, int y, double width, double height) {
+        lowerLeft = new Vector2D(x, y);
+        upperRight = new Vector2D(x + width, y + height);
     }
 
     public boolean collision(Player player) {
-        return false;
+        Vector2D playerPosition = player.getPosition();
+
+        if (playerPosition.getX() > upperRight.getX()) return false;
+        if (playerPosition.getX() + 1 < lowerLeft.getX()) return false;
+        if (playerPosition.getY() > upperRight.getY()) return false;
+        if (playerPosition.getY() + 1 < lowerLeft.getY()) return false;
+
+        return true;
     }
 
     public boolean topCollision(Player player) {
-        return false;
+        if (!collision(player)) return false;
+        return player.getPreviousPosition().getY() >= upperRight.getY();
     }
 }
