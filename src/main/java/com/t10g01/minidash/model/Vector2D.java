1375f19d215a1bb09eda6e841313ac2c4319fb83
Sofia Sousa <sofiasaesousa@gmail.com>
2024-12-05 14:39:55 +0000

    Vector2D rotation skeleton and tests

--- src/main/java/com/t10g01/minidash/model/Vector2D.java
+++ src/main/java/com/t10g01/minidash/model/Vector2D.java
@@ -19,6 +19,10 @@ public class Vector2D {
         return y;
     }
 
+    public void rotate(double angle) {
+        // implement
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
---
+++ src/test/groovy/com/t10g01/minidash/model/Vector2DSpec.groovy
@@ -0,0 +1,30 @@
+package com.t10g01.minidash.model
+
+import spock.lang.Specification
+
+class Vector2DSpec extends Specification {
+
+    def 'vector rotation around (0, 0)'(xi, yi, angle, xf, yf) {
+        given:
+        def vector = new Vector2D(xi, yi)
+
+        when:
+        vector.rotate(angle)
+
+        then:
+        vector.getX() == xf
+        vector.getY() == yf
+
+        where:
+        xi | yi | angle | xf                | yf
+        1  | 0  | 90    | 0                 | 1
+        1  | 0  | -90   | 0                 | -1
+        1  | 0  | 270   | 0                 | -1
+        1  | 0  | 180   | -1                | 0
+        1  | 0  | 360   | 0                 | 1
+        1  | 0  | 45    | Math.sqrt(2) / 2  | Math.sqrt(2) / 2
+        1  | 0  | 30    | Math.sqrt(3) / 2  | 1 / 2
+        1  | 0  | 60    | 1 / 2             | Math.sqrt(3) / 2
+
+    }
+}
