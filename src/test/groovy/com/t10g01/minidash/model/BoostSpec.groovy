222a07c3e6c68f20018f4f51fe477c59be975b6d
Sofia Sousa <sofiasaesousa@gmail.com>
2024-12-12 17:06:45 +0000

    boost model tests

--- src/test/groovy/com/t10g01/minidash/model/BoostSpec.groovy
+++ src/test/groovy/com/t10g01/minidash/model/BoostSpec.groovy
@@ -5,12 +5,51 @@ import spock.lang.Specification
 
 class BoostSpec extends Specification {
 
-    def 'boost collision checks boundingBox first'() {
-        // TODO add tests
+    @Shared
+    BoxCollider boundingBox, collider1, collider2
+    @Shared
+    Boost boost
+    @Shared
+    Player player
+
+    def setup() {
+        boundingBox = Mock(BoxCollider)
+        collider1 = Mock(BoxCollider)
+        collider2 = Mock(BoxCollider)
+        boost = new Boost(boundingBox, Arrays.asList(collider1, collider2))
+        player = Mock(Player)
     }
 
-    def 'collision with boost colliders'() {
-        // TODO add tests
+    def 'boost collision checks boundingBox first'() {
+        given:
+        boundingBox.collision(player) >> false
+        collider1.collision(player) >> true
+        collider2.collision(player) >> true
+
+        when:
+        boost.collision(player)
+
+        then:
+        1 * boundingBox.collision(player)
+        0 * collider1.collision(player)
+        0 * collider2.collision(player)
+    }
+
+    def 'collision with boost colliders'(a, b, result) {
+        given:
+        boundingBox.collision(player) >> true
+        collider1.collision(player) >> a
+        collider2.collision(player) >> b
+
+        expect:
+        boost.collision(player) == result
+
+        where:
+        a     | b     | result
+        false | false | false
+        false | true  | true
+        true  | false | true
+        true  | true  | true
     }
 
 }
