7d987d168aaa35b046869e2b09444bbd77706749
LuisG05 <lumicosgon.lg@gmail.com>
2024-12-23 17:21:21 +0000

    Killed mutants in LevelState and added tests

--- src/test/groovy/com/t10g01/minidash/state/LevelStateSpec.groovy
+++ src/test/groovy/com/t10g01/minidash/state/LevelStateSpec.groovy
@@ -1,5 +1,6 @@
 package com.t10g01.minidash.state
 
+import com.t10g01.minidash.Game
 import com.t10g01.minidash.model.Block
 import com.t10g01.minidash.model.Boost
 import com.t10g01.minidash.model.LevelEnd
@@ -7,27 +8,54 @@ import com.t10g01.minidash.model.DoubleJump
 import com.t10g01.minidash.model.Platform
 import com.t10g01.minidash.model.ReversedSpike
 import com.t10g01.minidash.model.Spike
+import com.t10g01.minidash.utils.GameSettings
 import spock.lang.Specification
 
 class LevelStateSpec extends Specification {
+
+    def "parses level correctly"() {
+        when:
+        def game = Mock(Game)
+        def settings = Mock(GameSettings)
+        settings.getRotationSpeed() >> 0
+        game.getGameSettings() >> settings
+        def levelState = new LevelState(game)
+
+        then:
+        levelState.model.elements == [new Platform(0, 0), new Boost(0, 1), new ReversedSpike(1, 0), new Spike(1, 1), new DoubleJump(1, 2)]
+    }
+
     def "parses big string correctly"() {
         expect:
             LevelState.getElement(str, x, y) == object;
         where:
-            str             | x  | y  | object
-            "#| >1h #>)*<_" | 1  | 0  | new Block(1, 0)
-            "#| >1h #>)*<_" | 10 | 1  | new Platform(10, 1)
-            "#| >1h #>)*<_" | 0  | 2  | null
-            "#| >1h #>)*<_" | 1  | 3  | new Spike(1, 3)
-            "#| >1h #>)*<_" | 20 | 4  | null
-            "#| >1h #>)*<_" | 1  | 5  | null
-            "#| >1h #>)*<_" | 2  | 6  | null
-            "#| >1h #>)*<_" | 3  | 7  | new Block(3,7)
-            "#| >1h #>)*<_" | 42 | 8  | new Spike(42, 8)
-            "#| >1h #>)*<_" | 2  | 9  | new Boost(2, 9)
-            "#| >1h #>)*<_" | 45 | 10 | new DoubleJump(45, 10)
-            "#| >1h #>)*<_" | 78 | 11 | new ReversedSpike(78, 11)
-            "#| >1h #>)*<_" | 99 | 12 | new LevelEnd(99, 12)
+            str           | x  | y  | object
+            "#| > #>)*<_" | 1  | 0  | new Block(1, 0)
+            "#| > #>)*<_" | 10 | 1  | new Platform(10, 1)
+            "#| > #>)*<_" | 0  | 2  | null
+            "#| > #>)*<_" | 1  | 3  | new Spike(1, 3)
+            "#| > #>)*<_" | 2  | 4  | null
+            "#| > #>)*<_" | 3  | 5  | new Block(3,5)
+            "#| > #>)*<_" | 42 | 6  | new Spike(42, 6)
+            "#| > #>)*<_" | 2  | 7  | new Boost(2, 7)
+            "#| > #>)*<_" | 45 | 8  | new DoubleJump(45, 8)
+            "#| > #>)*<_" | 78 | 9  | new ReversedSpike(78, 9)
+            "#| > #>)*<_" | 99 | 10 | new LevelEnd(99, 10)
+    }
+
+    def "parses big string correctly"() {
+        when:
+        LevelState.getElement(str, x, y)
+
+        then:
+        thrown(IllegalArgumentException)
+
+        where:
+        str   | x  | y
+        "q1+" | 1  | 0
+        "q1+" | 42 | 1
+        "q1+" | 99 | 2
+
     }
 
     def "throws in null string"() {
